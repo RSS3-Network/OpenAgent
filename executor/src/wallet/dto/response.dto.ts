1170fecd8fa34305def909beaab1678bfd96560d
pseudoyu <pseudoyu@connect.hku.hk>
2024-03-11 15:16:44 +0800

    refactor: rename to executor api

--- executor/src/wallet/dto/response.dto.ts
+++
@@ -1,5 +0,0 @@
-export class ResponseDto {
-  data: {
-    items: any[];
-  };
-}
--- executor/src/wallet/wallet.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { WalletController } from './wallet.controller';
-import { WalletService } from './wallet.service';
-
-@Module({
-  controllers: [WalletController],
-  providers: [WalletService],
-})
-export class WalletModule {}
