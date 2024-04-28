611325498b27a046673a284ace27951bee1d3278
BirdRing <zhkkun@gmail.com>
2024-04-29 02:35:52 +0800

    chore: replace feed_expert api endpoint

--- backend/openagent/experts/feed_expert.py
+++ backend/openagent/experts/feed_expert.py
@@ -15,23 +15,17 @@ class ParamSchema(BaseModel):
         description="""wallet address or blockchain domain name,\
 hint: vitalik's address is vitalik.eth"""
     )
-    platform: str = Field(
-        description="platform filter, default is '', option: lens, mirror, uniswap.",
-        default="",
-    )
 
 
 class FeedExpert(BaseTool):
     name = "feed"
     description = """Use this tool to get the activities of a wallet address or \
-blockchain domain name on a platform, and know
- what this address has done or doing recently."""
+blockchain domain name and know what this address has done or doing recently."""
     args_schema: Type[ParamSchema] = ParamSchema
 
     def _run(
         self,
         address: str,
-        platform: str = "",
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
         raise NotImplementedError
@@ -39,15 +33,14 @@ blockchain domain name on a platform, and know
     async def _arun(
         self,
         address: str,
-        platform: str = "",
         run_manager: Optional[AsyncCallbackManagerForToolRun] = None,
     ):
-        return await fetch_feeds(address, platform)
+        return await fetch_feeds(address)
 
 
-async def fetch_feeds(address: str, platform: str):
-    host = "https://api.rss3.io/v1/notes/"
-    url = f"""{host}{address}?limit=5&platform={platform}"""
+async def fetch_feeds(address: str):
+    host = "https://testnet.rss3.io/data/accounts"
+    url = f"""{host}/{address}/activities?limit=10&action_limit=5&direction=out"""
     headers = {"Accept": "application/json"}
     async with aiohttp.ClientSession() as session:
         logger.info(f"fetching {url}")
