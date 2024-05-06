719cdbfe0bf1db3c35b41da52b8455e87b2ce19f
Thomas <wxy_000000@qq.com>
2024-05-06 15:20:57 +0800

    chore: add feed scraping functionality
    - Added feed scraping functionality to fetch feeds from Mirror and IQWiki platforms
    - Updated env.py to include RSS3_DATA_API endpoint
    - Added .env.example entry for RSS3_DATA_API server endpoint

--- backend/openagent/conf/env.py
+++ backend/openagent/conf/env.py
@@ -8,11 +8,12 @@ load_dotenv()
 class Settings(BaseSettings):
     MODEL_NAME: str = Field(default="llava:13b", env="MODEL_NAME")
     LLM_API_BASE: str = Field(..., env="LLM_API_BASE")
-    RSS3_AI_API_BASE: str = Field(..., env="RSS3_AI_API_BASE")
-    EXECUTOR_API: str = Field(..., env="EXECUTOR_API")
     NFTSCAN_API_KEY: str = Field(..., env="NFTSCAN_API_KEY")
     BIZ_DB_CONNECTION: str = Field(..., env="BIZ_DB_CONNECTION")
     VEC_DB_CONNECTION: str = Field(..., env="VEC_DB_CONNECTION")
+    RSS3_DATA_API: str = Field(
+        default="https://testnet.rss3.io/data", env="RSS3_DATA_API"
+    )
 
 
 settings = Settings()
---
+++ backend/openagent/index/feed_scrape.py
@@ -0,0 +1,45 @@
+import datetime
+import json
+
+import requests
+
+from openagent.conf.env import settings
+
+
+def fetch_mirror_feeds(since_timestamp, until_timestamp, limit=10) -> dict:
+    """
+    Fetch feeds from Mirror.
+    """
+    return fetch_feeds("Mirror", since_timestamp, until_timestamp, limit)
+
+
+def fetch_iqwiki_feeds(since_timestamp, until_timestamp, limit=10) -> dict:
+    """
+    Fetch feeds from IQWiki.
+    """
+    return fetch_feeds("IQ.Wiki", since_timestamp, until_timestamp, limit)
+
+
+def fetch_feeds(platform, since_timestamp, until_timestamp, limit=10) -> dict:
+    """
+    Fetch feeds from a platform.
+    """
+
+    url = (
+        f"{settings.RSS3_DATA_API}/platforms/{platform}/activities?"
+        f"since_timestamp={since_timestamp}&until_timestamp={until_timestamp}&"
+        f"limit={limit}"
+    )
+
+    payload = {}  # type: ignore
+    headers = {}  # type: ignore
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return json.loads(response.text)
+
+
+if __name__ == "__main__":
+    curr_ts = int(datetime.datetime.now().timestamp())
+    feeds = fetch_iqwiki_feeds(0, curr_ts)
+    print(json.dumps(feeds, indent=4))
