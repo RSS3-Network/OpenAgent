3e71dabd490fe1e0e3ec9f33a4bd226f54d15fc0
Thomas <wxy_000000@qq.com>
2024-05-07 21:55:53 +0800

    fix: Correct import statement for OpenAIEmbeddings in pgvector_store.py

--- backend/openagent/index/pgvector_store.py
+++ backend/openagent/index/pgvector_store.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
+from langchain.embeddings import OpenAIEmbeddings
 from langchain_core.documents import Document
-from langchain_openai import OpenAIEmbeddings
 from langchain_postgres.vectorstores import PGVector
 
 from openagent.conf.env import settings
