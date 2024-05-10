3d42c24b3d81211c8d808cacbad3935b7a53c81a
Thomas <wxy_000000@qq.com>
2024-05-10 17:54:38 +0800

    chore: Created database if it does not exist in database.py.

--- backend/openagent/db/database.py
+++ backend/openagent/db/database.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import create_database, database_exists
 
 from openagent.conf.env import settings
 from openagent.db.models import Base
 
-engine = create_engine(
-    settings.BIZ_DB_CONNECTION, connect_args={"options": "-c timezone=utc"}
-)
+url = settings.BIZ_DB_CONNECTION
+
+if not database_exists(url):
+    create_database(url)
+engine = create_engine(url, connect_args={"options": "-c timezone=utc"})
 Base.metadata.create_all(bind=engine)  # type: ignore
 
 DBSession = sessionmaker(bind=engine)
