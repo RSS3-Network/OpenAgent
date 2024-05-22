bfc1c4b628d704633d8c9221396760a6dcaefaea
birdring <zhkkun@gmail.com>
2024-05-22 21:12:59 +0800

    feat: support google gemini (#12)
    * feat: support google gemini 1.5
    
    * chore: simplify env
    
    * chore: refactor
    
    * chore: README
    
    ---------
    
    Co-authored-by: Henry <hi@henry.wang>

--- backend/openagent/agent/function_agent.py
+++ backend/openagent/agent/function_agent.py
@@ -3,14 +3,12 @@ from langchain.chat_models import ChatOllama, ChatOpenAI
 from langchain.memory import ChatMessageHistory, ConversationBufferMemory
 from langchain.prompts import MessagesPlaceholder
 from langchain.schema import SystemMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
 from toolz import memoize
 
 from openagent.agent.cache import init_cache
 from openagent.agent.postgres_history import PostgresChatMessageHistory
-from openagent.agent.system_prompt import (
-    SYSTEM_PROMPT,
-    ollama_agent_kwargs,
-)
+from openagent.agent.system_prompt import SYSTEM_PROMPT, custom_agent_kwargs
 from openagent.conf.env import settings
 from openagent.experts.article_expert import ArticleExpert
 from openagent.experts.feed_expert import FeedExpert
@@ -23,19 +21,61 @@ from openagent.experts.transfer_expert import TransferExpert
 init_cache()
 
 
+def create_agent(experts, interpreter, agent_kwargs, memory, agent_type):
+    return initialize_agent(
+        experts,
+        interpreter,
+        agent=agent_type,
+        verbose=True,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+        handle_parsing_errors=True,
+    )
+
+
+def create_interpreter(model_name):
+    if model_name.startswith("gpt"):
+        return ChatOpenAI(
+            model=model_name,
+            openai_api_base=settings.LLM_API_BASE,
+            temperature=0.3,
+            streaming=True,
+        )
+    elif model_name.startswith("gemini"):
+        return ChatGoogleGenerativeAI(
+            model=model_name,
+            google_api_key=settings.GOOGLE_API_KEY,
+        )
+    else:
+        return ChatOllama(
+            model=model_name,
+            base_url=settings.LLM_API_BASE,
+        )
+
+
 @memoize
 def get_agent(session_id: str) -> AgentExecutor:
+    # Initialize message history based on session_id
     message_history = (
         get_msg_history(session_id) if session_id else ChatMessageHistory()
     )
-    agent_kwargs = {
-        "system_message": SystemMessage(content=SYSTEM_PROMPT),
-        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
-    }
+
+    # Define agent arguments
+    agent_kwargs = (
+        {
+            "system_message": SystemMessage(content=SYSTEM_PROMPT),
+            "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+        }
+        if settings.MODEL_NAME.startswith("gpt")
+        else custom_agent_kwargs
+    )
+
+    # Create conversation memory
     memory = ConversationBufferMemory(
         memory_key="memory", return_messages=True, chat_memory=message_history
     )
-    # load Experts as tools for the agent
+
+    # List of experts to be loaded
     experts = [
         SearchExpert(),
         FeedExpert(),
@@ -46,36 +86,18 @@ def get_agent(session_id: str) -> AgentExecutor:
         TransferExpert(),
     ]
 
-    if settings.MODEL_NAME.startswith("gpt"):
-        interpreter = ChatOpenAI(
-            model=settings.MODEL_NAME,
-            openai_api_base=settings.LLM_API_BASE,
-            temperature=0.3,
-            streaming=True,
-        )
-        return initialize_agent(
-            experts,
-            interpreter,
-            agent=AgentType.OPENAI_FUNCTIONS,
-            verbose=True,
-            agent_kwargs=agent_kwargs,
-            memory=memory,
-            handle_parsing_errors=True,
-        )
-    else:
-        interpreter = ChatOllama(
-            model=settings.MODEL_NAME,
-            base_url=settings.LLM_API_BASE,
-        )
-        return initialize_agent(
-            experts,
-            interpreter,
-            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
-            verbose=True,
-            agent_kwargs=ollama_agent_kwargs,
-            memory=memory,
-            handle_parsing_errors=True,
-        )
+    # Initialize interpreter
+    interpreter = create_interpreter(settings.MODEL_NAME)
+
+    # Define agent type based on model name
+    agent_type = (
+        AgentType.OPENAI_FUNCTIONS
+        if settings.MODEL_NAME.startswith("gpt")
+        else AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION
+    )
+
+    # Return created agent with appropriate arguments
+    return create_agent(experts, interpreter, agent_kwargs, memory, agent_type)
 
 
 # this function is used to get the chat history of a session from Postgres
--- backend/openagent/conf/env.py
+++ backend/openagent/conf/env.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from dotenv import load_dotenv
 from pydantic import Field
 from pydantic_settings import BaseSettings
@@ -8,6 +10,7 @@ load_dotenv()
 class Settings(BaseSettings):
     MODEL_NAME: str = Field(default="llava:13b", env="MODEL_NAME")
     LLM_API_BASE: str = Field(..., env="LLM_API_BASE")
+    GOOGLE_API_KEY: Optional[str] = Field(default=None)
     NFTSCAN_API_KEY: str = Field(..., env="NFTSCAN_API_KEY")
     BIZ_DB_CONNECTION: str = Field(..., env="BIZ_DB_CONNECTION")
     VEC_DB_CONNECTION: str = Field(..., env="VEC_DB_CONNECTION")
