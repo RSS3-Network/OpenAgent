15f21499025a7257ee065d4c801f303b5124ab2a
Thomas <wxy_000000@qq.com>
2024-06-13 15:18:31 +0800

    feat: add balance_tool.py (feat/bot)

--- backend/openagent/conf/env.py
+++ backend/openagent/conf/env.py
@@ -14,15 +14,12 @@ class Settings(BaseSettings):
     NFTSCAN_API_KEY: str = Field(..., env="NFTSCAN_API_KEY")
     BIZ_DB_CONNECTION: str = Field(..., env="BIZ_DB_CONNECTION")
     VEC_DB_CONNECTION: str = Field(..., env="VEC_DB_CONNECTION")
-    RSS3_DATA_API: str = Field(
-        default="https://testnet.rss3.io/data", env="RSS3_DATA_API"
-    )
-    RSS3_SEARCH_API: str = Field(
-        default="https://devnet.rss3.io/search", env="RSS3_SEARCH_API"
-    )
+    RSS3_DATA_API: str = Field(default="https://testnet.rss3.io/data", env="RSS3_DATA_API")
+    RSS3_SEARCH_API: str = Field(default="https://devnet.rss3.io/search", env="RSS3_SEARCH_API")
     TG_API_ID: str = Field(..., env="TG_API_ID")
     TG_API_HASH: str = Field(..., env="TG_API_HASH")
     TG_BOT_TOKEN: str = Field(..., env="TG_BOT_TOKEN")
+    COVALENT_API_KEY: str = Field(..., env="COVALENT_API_KEY")
 
 
 settings = Settings()
