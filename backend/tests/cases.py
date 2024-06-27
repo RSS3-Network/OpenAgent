45e0d884b8a28da16a0886eab1f322d2c2137e32
Thomas <wxy_000000@qq.com>
2024-06-27 19:27:35 +0800

    chore: Refactor test cases

--- backend/tests/cases.py
+++ backend/tests/cases.py
@@ -4,18 +4,18 @@ import time
 from openagent.agent.function_agent import get_agent
 
 question_list = [
-    "Hello?",
-    "What is the price of ETH?",
-    "What did vitalik.eth do recently?",
-    "Send 0.01 eth to vitalik.eth",
+    # "Hello?",
+    # "What is the price of ETH?",
+    # "What did vitalik.eth do recently?",
+    # "Send 0.01 eth to vitalik.eth",
     "Swap 1 eth to usdt",
-    "What is MODE chain?",
-    "Give me the bitcoin price chart",
-    "List some popular NFTs?",
-    "Can you recommend me some articles about web3",
-    "What's the largest dex with highest trading volume on Solana?",
-    "When ETH ETF 19b-4 forms approved?",
-    "Who are the main investors of EigenLayer?",
+    # "What is MODE chain?",
+    # "Give me the bitcoin price chart",
+    # "List some popular NFTs?",
+    # "Can you recommend me some articles about web3",
+    # "What's the largest dex with highest trading volume on Solana?",
+    # "When ETH ETF 19b-4 forms approved?",
+    # "Who are the main investors of EigenLayer?",
 ]
 
 
@@ -29,10 +29,15 @@ async def init():
     agent = get_agent("")
     for question in question_list:
         print(f"Question: {question}")
-        try:
-            await agent.arun(question)
-        except Exception as e:
-            print(f"Error: {e}")
+        # try:
+
+        await agent.ainvoke(
+            {"input": question, "session_id": "123"},
+            config={
+                "metadata": {"agentName": "openagent-backend", "userId": "123"},
+            },
+        )
+
         time.sleep(1)
 
         print("--------------")
