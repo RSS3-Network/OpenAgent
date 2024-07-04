222e60aef4f12a7e156baa7cf2c5c0b111833407
Thomas <wxy_000000@qq.com>
2024-07-04 16:30:10 +0800

    feat: Add chainlit data persistence

--- backend/openagent/ui/app.py
+++ backend/openagent/ui/app.py
@@ -1,37 +1,54 @@
-from typing import Optional
+import json
+from typing import Dict, Optional
 
 import chainlit as cl
-from chainlit.types import ThreadDict
+import chainlit.data as cl_data
+from chainlit.data.sql_alchemy import SQLAlchemyDataLayer
 from langchain.memory import ConversationBufferMemory
 from langchain.schema.runnable.config import RunnableConfig
+from langchain_core.messages import FunctionMessage
+from loguru import logger
 
 from openagent.agent.function_agent import get_agent
+from openagent.conf.env import settings
+
+# Set up the data layer
+cl_data._data_layer = SQLAlchemyDataLayer(conninfo=settings.VEC_DB_CONNECTION)
 
 
 def setup_runnable():
+    """Set up the runnable agent."""
     agent = get_agent("")
     cl.user_session.set("runnable", agent)
+
+
+def initialize_memory() -> ConversationBufferMemory:
+    """Initialize conversation memory."""
+    return ConversationBufferMemory(return_messages=True)
 
 
 @cl.oauth_callback
 def oauth_callback(
     provider_id: str,
     token: str,
-    raw_user_data: dict[str, str],
+    raw_user_data: Dict[str, str],
     default_user: cl.User,
 ) -> Optional[cl.User]:
+    """OAuth callback function."""
     return default_user
 
 
 @cl.on_chat_start
 async def on_chat_start():
-    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
+    """Callback function when chat starts."""
+    cl.user_session.set("memory", initialize_memory())
     setup_runnable()
 
 
 @cl.on_chat_resume
-async def on_chat_resume(thread: ThreadDict):
-    memory = ConversationBufferMemory(return_messages=True)
+async def on_chat_resume(thread: cl_data.ThreadDict):
+    """Callback function when chat resumes."""
+    memory = initialize_memory()
     root_messages = [m for m in thread["steps"]]
     for message in root_messages:
         if message["type"] == "user_message":
@@ -40,27 +57,51 @@ async def on_chat_resume(thread: ThreadDict):
             memory.chat_memory.add_ai_message(message["output"])
 
     cl.user_session.set("memory", memory)
-
     setup_runnable()
+
+
+async def handle_function_message(message: FunctionMessage, msg: cl.Message):
+    """Handle FunctionMessage type of messages."""
+    if message.name == "swap":
+        swap_dict = json.loads(message.content)
+        from_chain = swap_dict["chain_id"]
+        to_chain = swap_dict["chain_id"]
+        from_token_ = swap_dict["from_token_address"]
+        to_token = swap_dict["to_token_address"]
+        from_amount = swap_dict["amount"]
+
+        widget = (
+            f"""<iframe src="http://localhost:8001/swap?fromAmount={from_amount}&"""
+            f"""fromChain={from_chain}&fromToken={from_token_}&toChain={to_chain}&"""
+            f"""toToken={to_token}" width="400" height="700"></iframe>"""
+        )
+        await msg.stream_token(widget)
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
+    """Callback function to handle user messages."""
     memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
-
     runnable = cl.user_session.get("runnable")
 
-    res = cl.Message(content="")
+    msg = cl.Message(content="")
 
-    result = await runnable.ainvoke(
-        {"input": message.content},
-        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
-    )
+    try:
+        async for chunk in runnable.astream(
+            {"input": message.content}, config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler(stream_final_answer=True)])
+        ):
+            if "output" in chunk:
+                await msg.stream_token(chunk["output"])
+            elif "messages" in chunk:
+                for message in chunk["messages"]:
+                    if isinstance(message, FunctionMessage):
+                        await handle_function_message(message, msg)
+    except Exception as e:
+        logger.error(f"Error processing message: {e}")
 
-    await cl.Message(content=result["output"]).send()
-
+    await msg.send()
     memory.chat_memory.add_user_message(message.content)
-    memory.chat_memory.add_ai_message(res.content)
+    memory.chat_memory.add_ai_message(msg.content)
 
 
 if __name__ == "__main__":
