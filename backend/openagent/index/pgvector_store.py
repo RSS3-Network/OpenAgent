778b590b375a989429869af60c98fe5cfeb6cef9
Thomas <wxy_000000@qq.com>
2024-07-05 17:00:13 +0800

    chore: Add OpenAIEmbeddings for non gemini models

--- backend/openagent/index/pgvector_store.py
+++ backend/openagent/index/pgvector_store.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_google_vertexai import VertexAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain_postgres.vectorstores import PGVector
 
 from openagent.conf.env import settings
@@ -9,9 +10,10 @@ load_dotenv()
 
 def build_vector_store() -> PGVector:
     collection_name = "backend"
-    underlying_embeddings = VertexAIEmbeddings(
-        model_name="textembedding-gecko@003", project=settings.PROJECT_ID
-    )
+    if settings.MODEL_NAME.startswith("gemini"):
+        underlying_embeddings = VertexAIEmbeddings(model_name="textembedding-gecko@003", project=settings.PROJECT_ID)
+    else:
+        underlying_embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
     return PGVector(
         embeddings=underlying_embeddings,
         collection_name=collection_name,
