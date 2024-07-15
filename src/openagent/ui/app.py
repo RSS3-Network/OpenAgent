b456cf5f1b9668a2c044f9e18b9aba0aa52450b2
Thomas <wxy_000000@qq.com>
2024-07-15 17:25:06 +0800

    chore: Add agent token stream support

--- src/openagent/ui/app.py
+++ src/openagent/ui/app.py
@@ -13,6 +13,7 @@ from loguru import logger
 from openagent.conf.env import settings
 from openagent.conf.llm_provider import get_available_providers, set_current_llm
 from openagent.ui.profile import profile_name_to_provider_key, provider_to_profile
+from openagent.workflows.member import members
 from openagent.workflows.workflow import build_workflow
 
 # Set up the data layer
@@ -110,21 +111,38 @@ async def on_message(message: cl.Message):
     set_current_llm(provider_key)
 
     msg = cl.Message(content="")
+    agent_names = [member['name'] for member in members]
 
     # try:
-    async for chunk in runnable.astream(
+    async for event in runnable.astream_events(
             {"messages": [HumanMessage(content=message.content)]},
-            config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler(stream_final_answer=True)])
+            config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler(stream_final_answer=True)]),
+            version="v1",
     ):
-        try:
-            if 'fallback_agent' in chunk:
-                await msg.stream_token(chunk["fallback_agent"]['messages'][0].content)
-            elif "messages" in chunk:
-                for message in chunk["messages"]:
-                    if isinstance(message, FunctionMessage):
-                        await handle_function_message(message, msg)
-        except Exception as e:
-            logger.error(f"Error processing message: {e}")
+
+        kind = event["event"]
+        logger.info(event)
+        if kind == "on_chat_model_stream":
+            if event["metadata"]["langgraph_node"] in agent_names:
+                content = event["data"]["chunk"].content
+                if content:
+                    await msg.stream_token(content)
+                    # calls_ = event["data"]["chunk"].additional_kwargs['tool_calls']
+                    # for call in calls_:
+                    #     if call['function']['name'] == 'swap':
+                    #         swap_dict = json.loads(message.content)
+                    #         from_chain = swap_dict["chain_id"]
+                    #         to_chain = swap_dict["chain_id"]
+                    #         from_token_ = swap_dict["from_token_address"]
+                    #         to_token = swap_dict["to_token_address"]
+                    #         from_amount = swap_dict["amount"]
+                    #
+                    #         widget = (
+                    #             f"""<iframe src="/widget/swap?fromAmount={from_amount}&"""
+                    #             f"""fromChain={from_chain}&fromToken={from_token_}&toChain={to_chain}&"""
+                    #             f"""toToken={to_token}" width="400" height="700"></iframe>"""
+                    #         )
+                    #         await msg.stream_token(widget)
 
     await msg.send()
     memory.chat_memory.add_user_message(message.content)
