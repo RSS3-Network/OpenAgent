07abb78a9f23a49327222c7745fc7b5390255b08
Thomas <wxy_000000@qq.com>
2024-07-16 21:44:57 +0800

    chore: remove dependencies

--- src/widget/src/components/Swap.tsx
+++ src/widget/src/components/Swap.tsx
@@ -1,21 +1,22 @@
 /* eslint-disable no-console */
-
 function getQueryParams() {
-  const params = new URLSearchParams(window.location.search);
-  return {
-    fromAmount: params.get('fromAmount') || 11,
-    fromChain: params.get('fromChain') || 1,
-    fromToken: params.get('fromToken') || 'eth',
-    toChain: params.get('toChain') || 1,
-    toToken: params.get('toToken') || 'weth',
-  };
+    const params = new URLSearchParams(window.location.search);
+    return {
+        fromAmount: params.get('fromAmount') || 11,
+        fromChain: params.get('fromChain') || 1,
+        fromToken: params.get('fromToken') || 'eth',
+        toChain: params.get('toChain') || 1,
+        toToken: params.get('toToken') || 'weth',
+    };
 }
 
 export function Swap() {
-  const { fromAmount, fromChain, fromToken, toChain, toToken } = getQueryParams();
+    const {fromAmount, fromChain, fromToken, toChain, toToken} = getQueryParams();
 
-  return (
-    <>
-    swap</>
-  );
+    return (
+        <>
+
+            swap
+        </>
+    );
 }
