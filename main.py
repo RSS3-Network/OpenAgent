cb45ea40e52a66c469283200b6b183b5af28795c
birdringxD <zhkkun@gmail.com>
2024-08-06 18:51:41 +0800

    chore: host to '0.0.0.0' in uvicorn run

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from loguru import logger
 if __name__ == "__main__":
     load_dotenv()
     logger.info("Starting OpenAgent")
-    uvicorn.run("openagent.app:app", host="localhost", reload=False, port=8000)
+    uvicorn.run("openagent.app:app", host="0.0.0.0", reload=False, port=8000)
