7030dee0fcceb0145d12c8e57367bcd0d500af43
FrankLi123 <zoli@ucsd.edu>
2024-08-08 02:24:55 +0800

    fix: modify transfer-widget code to enable Ethereum token transactions and add pre-transaction checks

--- widget/src/App.tsx
+++ widget/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import '@rainbow-me/rainbowkit/styles.css';
-import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
+import {RainbowKitProvider, getDefaultConfig, getDefaultWallets} from '@rainbow-me/rainbowkit';
+import {createConfig, http, WagmiProvider} from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import {mainnet, sepolia} from 'wagmi/chains';
+
+let MAINNET_RPC_URL = process.env.REACT_APP_MAINNET_RPC_URL || "";
+let SEPOLIA_RPC_URL = process.env.REACT_APP_SEPOLIA_RPC_URL || "";
 
 /* eslint-disable no-console */
-
 export function App() {
 
   return <>
@@ -14,13 +16,31 @@ export function App() {
   </>
 }
 
-// Configuration for RainbowKit
-const config = getDefaultConfig({
-  appName: 'My RainbowKit App',
-  projectId: 'No_ID',
-    chains: [mainnet, polygon, optimism, arbitrum],
+const { connectors } = getDefaultWallets({
+  appName: "OpenAgent Widget",
+  appDescription: "Widget",
+  // appIcon: "",
+  projectId: "project id",
+})
+
+export const wagmiConfig = createConfig({
+  chains:[mainnet,sepolia],
+  connectors,
   ssr: true,
-});
+  transports: {
+    [mainnet.id]: http(MAINNET_RPC_URL, { batch: true }),
+    [sepolia.id]: http(SEPOLIA_RPC_URL, { batch: true }),
+  },
+  syncConnectedChain: true,
+})
+
+declare module "wagmi" {
+  interface Register {
+    config: typeof wagmiConfig
+  }
+}
+
+
 
 // Create a new QueryClient instance for React Query
 const queryClient = new QueryClient();
@@ -34,7 +54,7 @@ const queryClient = new QueryClient();
  */
 export function TransferWidgetApp({ children }: { children: React.ReactNode }) {
   return (
-      <WagmiProvider config={config}>
+      <WagmiProvider config={wagmiConfig}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           {children}
--- widget/src/components/TransferWidget.tsx
+++ widget/src/components/TransferWidget.tsx
@@ -1,141 +1,231 @@
-import React, { useState, useEffect } from 'react';
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import React, {useState, useEffect} from 'react';
+import {ConnectButton} from '@rainbow-me/rainbowkit';
 import {
-  useSendTransaction,
-  useWaitForTransactionReceipt,
-  useAccount,
-  useBalance,
-  useConfig,
-  type BaseError
+    useSendTransaction,
+    useWaitForTransactionReceipt,
+    useWriteContract,
+    useAccount,
+    useBalance,
+    useEstimateGas,
+    useFeeData,
+    type BaseError
 } from 'wagmi';
-import { parseUnits } from 'viem';
+import {erc20Abi, encodeFunctionData, parseUnits, parseAbi, parseEther, formatEther, formatUnits} from 'viem';
 import styles from './TransferWidget.module.css';
 
 // Define the prop types for the TransferWidget component
 interface TransferWidgetProps {
-  token: string;
-  amount: string;
-  toAddress: string;
+    token: string,
+    amount: string,
+    toAddress: string,
+    tokenAddress?: string
 }
 
+
 /**
- * TransferWidgetComponent - A component for handling cryptocurrency transfers.
+ * TransferWidgetComponent - A component for handling cryptocurrency token transfers.
  */
 const TransferWidgetComponent: React.FC<TransferWidgetProps> = ({
-  token,
-  amount: initialAmount,
-  toAddress: initialToAddress,
-}) => {
-  const [currentAmount, setCurrentAmount] = useState(initialAmount);
-  const [currentToAddress, setCurrentToAddress] = useState(initialToAddress);
-  const [account, setAccount] = useState<string | null>(null);
-  const [status, setStatus] = useState<string>('');
-  const { address } = useAccount();
-  const config = useConfig();
-  const { data: balance } = useBalance({ address, token: token as `0x${string}` });
+                                                                    token,
+                                                                    tokenAddress,
+                                                                    amount,
+                                                                    toAddress,
+                                                                }) => {
+    const [currentAmount, setCurrentAmount] = useState(amount);
+    const [currentToAddress, setCurrentToAddress] = useState(toAddress);
+    const [account, setAccount] = useState<string | null>(null);
+    const [estimatedGasFee, setEstimatedGasFee] = useState<string>('0');
+    const [estimationError, setEstimationError] = useState<string | null>(null);
+    const [status, setStatus] = useState<string>('');
+    const {address} = useAccount();
+    const { writeContract, data: hash2, error: error2, isPending: isPending2 } = useWriteContract();
+    const [isAmountValid, setIsAmountValid] = useState(true);
 
-  const {
-    data: hash,
-    error,
-    isPending,
-    sendTransaction
-  } = useSendTransaction();
+    const {
+        data: hash1,
+        error: error1,
+        isPending: isPending1,
+        sendTransaction
+    } = useSendTransaction();
 
-  const {
-    isLoading: isConfirming,
-    isSuccess: isConfirmed
-  } = useWaitForTransactionReceipt({ hash });
+	const isErc20 = tokenAddress !== "0x0000000000000000000000000000000000000000";
+    const hash = isErc20 ? hash2 : hash1;
+	const error = isErc20 ? error2 : error1;
+	const isPending = isErc20 ? isPending2 : isPending1;
 
-  useEffect(() => {
-    if (address) {
-      setAccount(address);
-    }
-  }, [address]);
+    const {data: balance, isError: balanceError, isLoading: balanceLoading} = useBalance({
+        address,
+        token: !isErc20? undefined : tokenAddress as `0x${string}`,
+    });
+    const {
+        isLoading: isConfirming,
+        isSuccess: isConfirmed
+    } = useWaitForTransactionReceipt({hash});
 
-   /**
-   * Handle form submission for the transfer.
-   */
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (account) {
-      sendTransaction({
+    const { data: estimatedGas, isError: isEstimateError } = useEstimateGas({
         to: currentToAddress as `0x${string}`,
-        value: parseUnits(currentAmount, balance?.decimals || 18),
-      });
-    } else {
-      setStatus('Wallet is not connected');
+        value: parseUnits(currentAmount || '0', balance?.decimals || 18),
+        data: isErc20 ? encodeFunctionData({
+            abi: erc20Abi,
+            functionName: 'transfer',
+            args: [currentToAddress as `0x${string}`, parseUnits(currentAmount || '0', balance?.decimals || 18)]
+        }) : undefined,
+    });
+
+
+    // Feture the fee data
+    const { data: feeData } = useFeeData();
+
+    // Calculate gas fee
+    useEffect(() => {
+        // console.log("estimatedGas is:", estimatedGas);
+        // console.log("feeData is:", feeData);
+
+        if (estimatedGas && feeData?.maxFeePerGas) {
+            const gasFeeInWei = estimatedGas * feeData.maxFeePerGas;
+            const gasFeeInEth = formatEther(gasFeeInWei);
+            console.log("Gas fee in ETH:", gasFeeInEth);
+            setEstimatedGasFee(Number(gasFeeInEth).toFixed(8));
+        } else {
+            console.log("Missing estimatedGas or feeData.maxFeePerGas");
+            setEstimatedGasFee('0.00000000');
+        }
+    }, [estimatedGas, feeData]);
+
+    // Check if amount and gas fee is greater than balance
+     useEffect(() => {
+        if (balance && currentAmount && estimatedGasFee) {
+            const amountWei = parseUnits(currentAmount, balance.decimals);
+            const gasFeeWei = parseUnits(estimatedGasFee, 18); // Gas is always in ETH (18 decimals)
+            const totalCostWei = amountWei + gasFeeWei;
+            setIsAmountValid(totalCostWei <= balance.value);
+        }
+    }, [balance, currentAmount, estimatedGasFee]);
+
+     useEffect(() => {
+        setAccount(address as `0x${string}`)
+    }, [address]);
+
+     const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value === '' || /^\d*\.?\d*$/.test(value)) {
+        setCurrentAmount(value);
     }
-  };
+};
 
+    /**
+     * Handle form submission for the transfer.
+     */
+     const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (address && currentToAddress) {
+            const value = parseUnits(currentAmount, balance?.decimals || 18);
+       		if (!isErc20) {
+                    sendTransaction(
+                        {
+                            to: currentToAddress as `0x${string}`,
+                            value: parseUnits(currentAmount, balance?.decimals || 18),
+                        })
+            } else {
+                  writeContract({
+					abi: erc20Abi,
+					address: tokenAddress as `0x${string}`,
+					args: [currentToAddress as `0x${string}`, parseEther(String(value))],
+					functionName: "transfer",
+				},
+				{
+					onError: (error) => {
+						console.log(error, 111);
+					},
+				})
+            }
+        } else {
+            setStatus('Wallet is not connected or recipient address is missing');
+        }
+    };
 
-  // Render the component
-  return (
-    <div className={styles.transferWidget}>
-      <h3>Send</h3>
-      <div className={styles.accountInfo}>
-        <p>From</p>
-        <p>{account || 'Not connected'}</p>
-      </div>
-      <div className={styles.assetInfo}>
-        <label>Asset:</label>
-        <div className={styles.assetDetails}>
-          <span>{token}</span>
+    // Render the component
+    return (
+        <div className={styles.transferWidget}>
+            <h3>Send</h3>
+            <div className={styles.accountInfo}>
+                <p>From</p>
+                <p>{account || 'Not connected'}</p>
+            </div>
+            <div className={styles.assetInfo}>
+                <div className={styles.assetLabel}>
+                    <span>Asset:</span>
+                    <span>{token}</span>
+                </div>
+                <div className={styles.balanceLabel}>
+                    <span>Balance:</span>
+                    <span>{balance?.formatted || 0} {token}</span>
+                </div>
+            </div>
+            <div className={styles.amountInfo}>
+                <label>Amount:</label>
+                <input
+                    type="text"
+                    value={currentAmount}
+                    onChange={handleAmountChange}
+                    placeholder="Enter amount"
+                />
+            </div>
+            <div className={styles.addressInfo}>
+                <label>To Address:</label>
+                <input
+                    type="text"
+                    value={currentToAddress}
+                    onChange={(e) => setCurrentToAddress(e.target.value)}
+                    placeholder="Enter recipient address"
+                />
+            </div>
+            <div className={styles.gasFeeInfo}>
+                <p>Estimated gas fee</p>
+                {isEstimateError ? (
+                    <p>Error estimating gas</p>
+                ) : (
+                    <p>{estimatedGasFee} ETH</p>
+                )}
+            </div>
+            <div className={styles.buttonGroup}>
+                <ConnectButton/>
+                <button className={styles.transferButton} onClick={handleSubmit}
+                        disabled={!account || isPending || isConfirming}>
+                    {isPending ? 'Confirming...' : isConfirming ? 'Processing...' : 'Send'}
+                </button>
+            </div>
+            {!isAmountValid && <p className={styles.errorMessage}>Insufficient balance for transfer and gas fee</p>}
+            <div className={styles.status}>{status}</div>
+            {hash && <div className={styles.transactionInfo}>Transaction Hash: {hash}</div>}
+            {isConfirming && <div className={styles.transactionInfo}>Waiting for confirmation...</div>}
+            {isConfirmed && <div className={styles.transactionInfo}>Transaction confirmed.</div>}
+            {error && (
+                <div className={styles.error}>Error: {(error as BaseError).shortMessage || error.message}</div>
+            )}
         </div>
-      </div>
-      <div className={styles.amountInfo}>
-        <label>Amount:</label>
-        <input
-          type="text"
-          value={currentAmount}
-          onChange={(e) => setCurrentAmount(e.target.value)}
-          placeholder="Enter amount"
-        />
-      </div>
-      <div className={styles.addressInfo}>
-        <label>To Address:</label>
-        <input
-          type="text"
-          value={currentToAddress}
-          onChange={(e) => setCurrentToAddress(e.target.value)}
-          placeholder="Enter recipient address"
-        />
-      </div>
-      <div className={styles.gasFeeInfo}>
-      </div>
-      <div className={styles.buttonGroup}>
-        <ConnectButton />
-        <button className={styles.transferButton} onClick={handleSubmit} disabled={!account || isPending || isConfirming}>
-          {isPending ? 'Confirming...' : isConfirming ? 'Processing...' : 'Send'}
-        </button>
-      </div>
-      <div className={styles.status}>{status}</div>
-      {hash && <div className={styles.transactionInfo}>Transaction Hash: {hash}</div>}
-      {isConfirming && <div className={styles.transactionInfo}>Waiting for confirmation...</div>}
-      {isConfirmed && <div className={styles.transactionInfo}>Transaction confirmed.</div>}
-      {error && (
-        <div className={styles.error}>Error: {(error as BaseError).shortMessage || error.message}</div>
-      )}
-    </div>
-  );
+    );
 };
 
 /**
  * TransferWidget - Wrapper component that parses URL parameters and renders TransferWidgetComponent.
  */
 export function TransferWidget() {
-  const params = new URLSearchParams(window.location.search);
-  const token = params.get('token') || '';
-  const amount = params.get('amount') || '';
-  const toAddress = params.get('toAddress') || '';
+    const params = new URLSearchParams(window.location.search);
+    const token = params.get('token') || '';
+    const tokenAddress = params.get('tokenAddress') || '';
+    const amount = params.get('amount') || '';
+    const toAddress = params.get('toAddress') || '';
 
-  // @ts-ignore
-  return (
-    <TransferWidgetComponent
-      token={token}
-      amount={amount}
-      toAddress={toAddress}
-    />
-  );
+    // @ts-ignore
+    return (
+        <TransferWidgetComponent
+            token={token}
+            tokenAddress={tokenAddress}
+            amount={amount}
+            toAddress={toAddress}
+        />
+    );
 }
 
 export default TransferWidget;
